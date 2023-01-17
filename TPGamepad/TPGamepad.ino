1b3d938ac4b251d6d0a173fe056f32063ddd54d6
mule <30447288+jiamule@users.noreply.github.com>
2023-01-17 23:39:38 +0800

    Update TPGamepad.ino
    把原作者的4个按钮改成了9个，并增加蓝牙名称修改

--- TPGamepad/TPGamepad.ino
+++ TPGamepad/TPGamepad.ino
@@ -1,18 +1,23 @@
 #include <Bounce2.h>      // https://github.com/thomasfredericks/Bounce2
 #include <BleGamepad.h>   // https://github.com/lemmingDev/ESP32-BLE-Gamepad
 
+//对应esp32的pin针脚
 #define BUTTON1_PIN 23
-#define BUTTON2_PIN 19
-#define BUTTON3_PIN 18
-#define BUTTON4_PIN 5
-
-#define DPAD_UP_PIN 22
-#define DPAD_DOWN_PIN 21
-#define DPAD_LEFT_PIN 32
+#define BUTTON2_PIN 22
+#define BUTTON3_PIN 21
+#define BUTTON4_PIN 19
+#define BUTTON5_PIN 18
+#define BUTTON6_PIN 17
+#define BUTTON7_PIN 16
+#define BUTTON8_PIN 4
+#define BUTTON9_PIN 5
+#define DPAD_UP_PIN 27
+#define DPAD_DOWN_PIN 26
+#define DPAD_LEFT_PIN 25
 #define DPAD_RIGHT_PIN 33
 //#define LED_PIN 13
-
-BleGamepad bleGamepad;       // Instantiate a BleGamepad object
+//蓝牙名称
+BleGamepad bleGamepad("esp32-gamepad", "esp32");        // Instantiate a BleGamepad object
 
 struct buttonHandler {
   Bounce *debouncer;
@@ -20,7 +25,7 @@ struct buttonHandler {
   bool isPressed;
 };
 
-buttonHandler faceButtons[4];
+buttonHandler faceButtons[9];
 buttonHandler dPad[4];
 
 void initButton(buttonHandler *handler, int buttonPin, uint64_t button, int intervalMs = 5) {
@@ -46,6 +51,11 @@ void setup()
   initButton(&faceButtons[1], BUTTON2_PIN, BUTTON_2);
   initButton(&faceButtons[2], BUTTON3_PIN, BUTTON_3);
   initButton(&faceButtons[3], BUTTON4_PIN, BUTTON_4);
+  initButton(&faceButtons[4], BUTTON5_PIN, BUTTON_5);
+  initButton(&faceButtons[5], BUTTON6_PIN, BUTTON_6);
+  initButton(&faceButtons[6], BUTTON7_PIN, BUTTON_7);
+  initButton(&faceButtons[7], BUTTON8_PIN, BUTTON_8);
+  initButton(&faceButtons[8], BUTTON9_PIN, BUTTON_9);
 
   //Face Buttons
   initButton(&dPad[0], DPAD_UP_PIN, 0); // We won't send a button when a press is detected
@@ -112,7 +122,7 @@ void loop()
   if (bleGamepad.isConnected())
   {
     //Face Buttons
-    for (i = 0; i < 4; i++)
+    for (i = 0; i < 9; i++)
     {
       processButtonPress(&faceButtons[i]);
     }
